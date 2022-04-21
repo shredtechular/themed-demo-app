163dd0bc88df005226ad348a391d2c3ed8537f5a
Sara Mazer <smazer888@gmail.com>
2022-04-21 15:10:03 -0400

    cleon fix plus New Relic

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ import App from './App.js';
   const draw = await fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=1');
   const hand = await draw.json();
   const card = hand.cards[0];
+  var deviceType = '';
+
+
+
+  if ( /NewRelic/.test(navigator.userAgent) ) 
+  {
+    deviceType = 'NewRelic';
+  }
+  else {
+   deviceType = 'humans';
+  }
+  console.log('users device is:', deviceType);
 
   const LDProvider = await asyncWithLDProvider({
     clientSideID: process.env.REACT_APP_LD_CLIENT_SIDE_ID,
@@ -17,6 +29,7 @@ import App from './App.js';
       key: card.code,
       avatar: card.image,
       custom: {
+        groups: deviceType,
         card: card.code,
         suit: card.suit,
         face: card.value,
